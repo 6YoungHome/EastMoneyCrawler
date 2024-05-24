efc80059f1a3ede0535130be33bfa4222d1a9a65
6YoungHome <15234015704@163.com>
2024-05-24 16:51:56 +0800

    selenium的服务器适应

--- crawler.py
+++ crawler.py
@@ -24,6 +24,8 @@ class Crawler:
         
     def create_webdriver(self):
         options = webdriver.ChromeOptions()  # configure the webdriver
+        options.add_argument('no-sandbox')
+        options.add_argument('disable-dev-shm-usage')
         options.add_argument('lang=zh_CN.UTF-8')
         options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
                              'like Gecko) Chrome/111.0.0.0 Safari/537.36"')
